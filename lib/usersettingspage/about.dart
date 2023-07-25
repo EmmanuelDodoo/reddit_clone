3a8d64fcc1fa8463aed0772810ca79f288838825
emmanuel dodoo <end25@cornell.edu>
2023-07-25 13:19:04 -0700

    Completed About section

--- lib/usersettingspage/about.dart
+++ lib/usersettingspage/about.dart
@@ -1,14 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:reddit_clone/models/api/http_model.dart';
 import 'package:reddit_clone/models/user.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../models/inherited-data.dart';
 import '../models/userprovider.dart';
 
 class About extends StatelessWidget {
   About({Key? key}) : super(key: key);
+  final String aboutText =
+      """Emmanuel Dodoo here. I'm, as of the last update, a passionate college student from Ghana. I began this Reddit clone to immerse myself in the world of mobile development and maybe find a calling in the field. The journey was long but I embraced the challenges and victories, crafting a feature-rich app that I'm proud of. Although satisfied with its current state, I think there's ample room for improvement, and I look forward to returning to implement more exciting features in the future. This project tested my perseverance and sanity in a way I never though possible. Was it worth it? For me, yes. Yes it was. To catch up on what I've been working on, Click the Resume option below. """;
+
+  final String resumeURL =
+      "https://emmanueldodoo.com/Emmanuel-Dodoo-Resume.pdf";
 
   User? _currUser;
+
+  void _handleResume(BuildContext context) async {
+    var url = Uri.parse(resumeURL);
+    if (await launchUrl(url, mode: LaunchMode.externalApplication)) {}
+  }
+
+  void _aboutDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) => Dialog(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: Text(
+                        "Well Hello There",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ),
+                    Flexible(
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: 6.0),
+                        child: Text(
+                          aboutText,
+                          textAlign: TextAlign.justify,
+                          softWrap: true,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(letterSpacing: 0.4),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Thanks!!",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium
+                          ?.copyWith(letterSpacing: 0.5),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Close'),
+                    ),
+                  ],
+                ),
+              ),
+            ));
+  }
 
   Widget optionBuilder(String name, BuildContext context) {
     return Padding(
@@ -68,8 +130,6 @@ class About extends StatelessWidget {
     List<String> notificationOptions = [
       "Help Center",
       "Report Issue",
-      "About",
-      "Resume"
     ];
 
     return Padding(
@@ -95,7 +155,51 @@ class About extends StatelessWidget {
               ),
               ...notificationOptions
                   .map((opt) => optionBuilder(opt, context))
-                  .toList()
+                  .toList(),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: InkWell(
+                  onTap: () => _aboutDialog(context),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "About",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(fontWeight: FontWeight.w500),
+                      ),
+                      const Icon(
+                        Icons.arrow_forward_rounded,
+                        size: 20,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: InkWell(
+                  onTap: () => _handleResume(context),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Resume",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(fontWeight: FontWeight.w500),
+                      ),
+                      const Icon(
+                        Icons.arrow_forward_rounded,
+                        size: 20,
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
